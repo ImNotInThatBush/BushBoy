81acb7a0393b28d9fa6f33280115a7ff36c6da0c
ImNotInThatBush <imnotinthatbush@gmail.com>
2025-05-22 14:33:09 +0200

    Fix type error: pass Option<Vec<Key>> to handle_keys()

--- gui/src/main.rs
+++ gui/src/main.rs
@@ -1,12 +1,12 @@
-use emulator_core::Emulator;
 use minifb::{Key, Window, WindowOptions};
+use emulator_core::Emulator;
 
 const WIDTH: usize = 160;
 const HEIGHT: usize = 144;
 
 fn main() {
-    let mut emulator = Emulator::new();
     let mut buffer: Vec<u32> = vec![0; WIDTH * HEIGHT];
+    let mut emulator = Emulator::new();
 
     let mut window = Window::new(
         "BushBoy - Press ESC to exit",
@@ -19,9 +19,6 @@ fn main() {
     while window.is_open() && !window.is_key_down(Key::Escape) {
         let keys = window.get_keys();
         emulator.handle_keys(&keys);
-
-        emulator.tick();
-        emulator.render(&mut buffer);
 
         window
             .update_with_buffer(&buffer, WIDTH, HEIGHT)
