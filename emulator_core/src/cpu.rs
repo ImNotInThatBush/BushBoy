76678194af877a96a2b81abbeb595ae95021286b
ImNotInThatBush <imnotinthatbush@gmail.com>
2025-05-22 04:12:34 +0200

    Fix unclosed delimiter and underscore; full working cpu.rs with INC B

--- emulator_core/src/cpu.rs
+++ emulator_core/src/cpu.rs
@@ -67,4 +67,59 @@ impl CPU {
                 let prev = self.b;
                 self.b = self.b.wrapping_add(1);
                 self.set_flag(7, self.b == 0);                     // Z
-                self.set_flag(6, false);_
+                self.set_flag(6, false);                           // N
+                self.set_flag(5, (prev & 0x0F) + 1 > 0x0F);        // H
+                println!("INC B -> {:02X}", self.b);
+                self.pc += 1;
+            }
+            0x11 => {
+                let lo = mem.read_byte(self.pc + 1);
+                let hi = mem.read_byte(self.pc + 2);
+                self.e = lo;
+                self.d = hi;
+                println!("LD DE, ${:02X}{:02X}", self.d, self.e);
+                self.pc += 3;
+            }
+            0x21 => {
+                let lo = mem.read_byte(self.pc + 1);
+                let hi = mem.read_byte(self.pc + 2);
+                self.l = lo;
+                self.h = hi;
+                println!("LD HL, ${:02X}{:02X}", self.h, self.l);
+                self.pc += 3;
+            }
+            0x31 => {
+                let lo = mem.read_byte(self.pc + 1);
+                let hi = mem.read_byte(self.pc + 2);
+                self.sp = ((hi as u16) << 8) | lo as u16;
+                println!("LD SP, ${:04X}", self.sp);
+                self.pc += 3;
+            }
+            0xEA => {
+                let lo = mem.read_byte(self.pc + 1);
+                let hi = mem.read_byte(self.pc + 2);
+                let addr = ((hi as u16) << 8) | lo as u16;
+                mem.write_byte(addr, self.a);
+                println!("LD (${:04X}), A -> Wrote {:02X}", addr, self.a);
+                self.pc += 3;
+            }
+            0xC3 => {
+                let lo = mem.read_byte(self.pc + 1);
+                let hi = mem.read_byte(self.pc + 2);
+                let addr = ((hi as u16) << 8) | lo as u16;
+                println!("JP ${:04X}", addr);
+                self.pc = addr;
+            }
+            _ => {
+                println!("Unknown opcode: {:02X}", opcode);
+                self.pc += 1;
+            }
+        }
+    }
+
+    pub fn run(&mut self, mem: &mut Memory, steps: usize) {
+        for _ in 0..steps {
+            self.step(mem);
+        }
+    }
+}
