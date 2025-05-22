2fc8589935d5c36a63f32e26bb3c838408daa921
ImNotInThatBush <imnotinthatbush@gmail.com>
2025-05-22 14:01:39 +0200

    Add stub implementation for Emulator in emulator_core

--- emulator_core/src/lib.rs
+++ emulator_core/src/lib.rs
@@ -1,5 +1,22 @@
-pub mod cpu;
-pub mod memory;
+pub struct Emulator;
 
-pub use cpu::CPU;
-pub use memory::Memory;
+impl Emulator {
+    pub fn new() -> Self {
+        Emulator
+    }
+
+    pub fn tick(&mut self) {
+        // TODO: implement CPU cycle
+    }
+
+    pub fn render(&self, buffer: &mut [u32]) {
+        // TODO: draw to screen buffer
+        for pixel in buffer.iter_mut() {
+            *pixel = 0x00_00_FF; // blu (BGR format)
+        }
+    }
+
+    pub fn handle_keys(&mut self, _keys: &Option<Vec<minifb::Key>>) {
+        // TODO: handle keyboard input
+    }
+}
