0f66dde6eb3082b9d2073376f801239faa8c7ee2
ImNotInThatBush <imnotinthatbush@gmail.com>
2025-05-22 12:54:39 +0200

    Add VRAM dump after 500 CPU steps

--- gui/src/main.rs
+++ gui/src/main.rs
@@ -12,4 +12,10 @@ fn main() {
 
     cpu.run(&mut mem, 500);
     cpu.debug();
+
+    println!("\n=== VRAM Dump (0x8000 - 0x8010) ===");
+    for addr in 0x8000..=0x8010 {
+        let byte = mem.read_byte(addr);
+        println!("{:04X}: {:02X}", addr, byte);
+    }
 }
