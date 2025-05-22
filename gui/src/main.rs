8be9c0bb18f1c6c684c4707c44efb8bbf7241ec2
ImNotInThatBush <imnotinthatbush@gmail.com>
2025-05-22 03:33:07 +0200

    Add ROM loading in gui

--- gui/src/main.rs
+++ gui/src/main.rs
@@ -1,4 +1,14 @@
+use std::fs::File;
+use std::io::Read;
+
 fn main() {
+    let mut file = File::open("snake.gb").expect("ROM file not found");
+    let mut rom_data = Vec::new();
+    file.read_to_end(&mut rom_data).expect("Failed to read ROM");
+
+    let mem = emulator_core::memory::Memory::new(rom_data);
+    println!("ROM loaded: {} bytes", mem.rom.len());
+
     let cpu = emulator_core::cpu::CPU::new();
     cpu.debug();
 }
