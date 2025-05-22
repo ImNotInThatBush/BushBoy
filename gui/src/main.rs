978accd09b3cc952147fd884acee9c47dc7ce6bb
ImNotInThatBush <imnotinthatbush@gmail.com>
2025-05-22 13:02:39 +0200

    Add minifb graphics output to main.rs

--- gui/src/main.rs
+++ gui/src/main.rs
@@ -1,6 +1,10 @@
 use emulator_core::{CPU, Memory};
 use std::fs::File;
 use std::io::Read;
+use minifb::{Key, Window, WindowOptions};
+
+const WIDTH: usize = 160;
+const HEIGHT: usize = 144;
 
 fn main() {
     let mut file = File::open("snake.gb").expect("ROM non trovata");
@@ -10,12 +14,28 @@ fn main() {
     let mut mem = Memory::new(buffer);
     let mut cpu = CPU::new();
 
-    cpu.run(&mut mem, 500);
-    cpu.debug();
+    let mut window = Window::new("BushBoy - Snake", WIDTH, HEIGHT, WindowOptions::default())
+        .expect("Impossibile creare finestra");
 
-    println!("\n=== VRAM Dump (0x8000 - 0x8010) ===");
-    for addr in 0x8000..=0x8010 {
-        let byte = mem.read_byte(addr);
-        println!("{:04X}: {:02X}", addr, byte);
+    let mut pixels = vec![0x00_00_00; WIDTH * HEIGHT];
+
+    while window.is_open() && !window.is_key_down(Key::Escape) {
+        cpu.run(&mut mem, 10);
+
+        for y in 0..HEIGHT {
+            for x in 0..WIDTH {
+                let addr = 0x8000 + (y * WIDTH + x) % 0x2000;
+                let val = mem.read_byte(addr);
+                let color = match val {
+                    0 => 0xFF_FF_FF,
+                    1..=63 => 0xAA_AA_AA,
+                    64..=127 => 0x55_55_55,
+                    _ => 0x00_00_00,
+                };
+                pixels[y * WIDTH + x] = color;
+            }
+        }
+
+        window.update_with_buffer(&pixels, WIDTH, HEIGHT).unwrap();
     }
 }
