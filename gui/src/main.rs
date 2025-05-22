ea911bc0f641d10546ec2a5b0b2c791225909501
ImNotInThatBush <imnotinthatbush@gmail.com>
2025-05-22 16:45:45 +0200

    fix: parametro handle_keys ora riceve &Option<Vec<Key>>

--- gui/src/main.rs
+++ gui/src/main.rs
@@ -1,23 +1,32 @@
-use emulator_core::Emulator;
 use minifb::{Key, Window, WindowOptions};
+use emulator_core::Emulator;
+
+const WIDTH: usize = 160;
+const HEIGHT: usize = 144;
 
 fn main() {
     let mut window = Window::new(
-        "BushBoy Emulator",
-        160,
-        144,
+        "Bush Boy Emulator",
+        WIDTH,
+        HEIGHT,
         WindowOptions::default(),
-    )
-    .unwrap_or_else(|e| panic!("{}", e));
+    ).unwrap_or_else(|e| {
+        panic!("{}", e);
+    });
 
-    let mut buffer: Vec<u32> = vec![0; 160 * 144];
+    let mut buffer: Vec<u32> = vec![0; WIDTH * HEIGHT];
     let mut emulator = Emulator::new();
 
     while window.is_open() && !window.is_key_down(Key::Escape) {
-        let keys = window.get_keys();
-        emulator.handle_keys(&Some(keys));
         emulator.tick();
         emulator.render(&mut buffer);
-        window.update_with_buffer(&buffer, 160, 144).unwrap();
+
+        // Passa &Option<Vec<Key>> come richiesto da handle_keys
+        let keys = window.get_keys();
+        emulator.handle_keys(&keys);
+
+        window
+            .update_with_buffer(&buffer, WIDTH, HEIGHT)
+            .unwrap();
     }
 }
