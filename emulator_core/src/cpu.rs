bf8302caeb210d8a335c10358db3104fd5f6aef8
ImNotInThatBush <imnotinthatbush@gmail.com>
2025-05-22 03:47:23 +0200

    Add CPU run() method for step loop

--- emulator_core/src/cpu.rs
+++ emulator_core/src/cpu.rs
@@ -52,4 +52,10 @@ impl CPU {
             }
         }
     }
+
+    pub fn run(&mut self, mem: &Memory, steps: usize) {
+        for _ in 0..steps {
+            self.step(mem);
+        }
+    }
 }
